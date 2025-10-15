e5a9cbbf2e224975fa80cfd0ae2f2fe4d6dbd8b3
donaldcjapi <dcjapi@gmail.com>
2025-10-15 07:06:25 +0700

    Initial Commit

---
+++ Quran Memorizer/Quran_MemorizerApp.swift
@@ -0,0 +1,32 @@
+//
+//  Quran_MemorizerApp.swift
+//  Quran Memorizer
+//
+//  Created by Donald Cjapi on 15/10/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Quran_MemorizerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
